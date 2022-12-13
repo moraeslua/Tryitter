7ca3932289664ddc8b55e6283d02e78e745a2e38
moraeslua <moraeslua66@gmail.com>
2022-12-13 12:00:27 -0300

    feat: student controller

---
+++ src/Controllers/StudentController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Tryitter.Dto;
+using Tryitter.Helpers;
+using Tryitter.Interfaces.Services;
+using Tryitter.src.Dto;
+using Tryitter.src.Interfaces.Services;
+
+namespace Tryitter.Controllers
+{
+    [ApiController]
+    [Route("students")]
+    public class StudentController : Controller
+    {
+        public IStudentService _studentService { get; private set; }
+        public ITokenService _tokenService { get; private set; }
+        public StudentController(IStudentService studentService, ITokenService tokenService)
+        {
+            _studentService = studentService;
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        [Route("register")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> RegisterAsync([FromBody] CreateStudentRequestDto request)
+        {
+            var result = await _studentService.Create(request);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        public async Task<ActionResult<dynamic>> Authenticate([FromBody] AuthorizeRequestDto request)
+        {
+            var result = await _studentService.Authenticate(request);
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        //[ProducesResponseType(typeof(Student), (int)HttpStatusCode.OK)]
+        //[ProducesResponseType((int)HttpStatusCode.NotFound)]
+        //[ProducesResponseType((int)HttpStatusCode.BadRequest)]
+        //[ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+        public async Task<IActionResult> GetAsync(Guid id)
+        {
+            var payload = Request.GetAuthenticationPayload();
+            var student = await _studentService.ReadById(payload.UserId);
+            return Ok(student);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var students = await _studentService.ReadAll();
+            return Ok(students);
+        }
+
+        [HttpPut("{id}")]
+        [Authorize]
+        public async Task<IActionResult> UpdateAsync([FromBody] UpdateStudentRequestDto request)
+        {
+            var payload = Request.GetAuthenticationPayload();
+            var studentUpdated = await _studentService.Update(payload.UserId, request);
+            return Ok(studentUpdated);
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var payload = Request.GetAuthenticationPayload();
+            await _studentService.Delete(payload.UserId);
+            return Ok();
+        }
+    }
+}
