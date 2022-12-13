da9b1635d63c27c82349d809b5b874a89c872bd1
moraeslua <moraeslua66@gmail.com>
2022-12-13 11:58:59 -0300

    feat: implements sql server configuration and repository

---
+++ src/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Tryitter.Models
+{
+    public class Student
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(300)]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(300)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Module { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string? Password { get; set; }
+
+        [JsonIgnore]
+        public List<Post>? Posts { get; set; }
+    }
+}
