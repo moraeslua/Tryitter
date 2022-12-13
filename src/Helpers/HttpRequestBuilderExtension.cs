d560966687e066ce9076774654c826c49a3fdee9
moraeslua <moraeslua66@gmail.com>
2022-12-13 12:00:18 -0300

    feat: implements http request extension to get payload from token

---
+++ src/Helpers/HttpRequestBuilderExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Claims;
+
+namespace Tryitter.Helpers
+{
+    public static class HttpRequestBuilderExtension
+    {
+        public static AuthenticationPayload GetAuthenticationPayload(this HttpRequest request)
+        {
+            var identitdy = request.HttpContext.User.Identity as ClaimsIdentity;
+            if (identitdy is not null)
+            {
+                var userId = identitdy.FindFirst("UserId")?.Value;
+                var email = identitdy.FindFirst(ClaimTypes.Email)?.Value;
+
+                return new AuthenticationPayload() { UserId = new Guid(userId!), Email = email! };
+            }
+            return new AuthenticationPayload();
+        }
+    }
+}
