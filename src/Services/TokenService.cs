c8f1c15ad722212728fc813451c7a52164f4bbbb
moraeslua <moraeslua66@gmail.com>
2022-12-13 11:59:21 -0300

    feat: jwt token service

---
+++ src/Services/TokenService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Tryitter.Interfaces.Services;
+using Tryitter.Models;
+
+namespace Tryitter.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string GenerateToken(Student student)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            byte[] key = Encoding.ASCII.GetBytes(Settings.JwtSecret);
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                new Claim("UserId", student.Id.ToString()),
+                new Claim(ClaimTypes.Email, student.Email),
+            }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature) // acho que compõe o header
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+};
